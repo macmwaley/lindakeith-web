7007f992185375b600e20c06e9346866e9cbfe0f
macmwaley <mcmwaley@gmail.com>
2026-06-07 13:05:27 +0300

    Remove typedRoutes experiment

--- next.config.js
+++ next.config.js
@@ -1,19 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Future Sanity image domains
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'cdn.sanity.io',
-        port: '',
         pathname: '/images/**',
       },
     ],
-  },
-  // Experimental features for Next.js 15
-  experimental: {
-    typedRoutes: true,
   },
 }
 
